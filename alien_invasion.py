8d1fa6e3cc5dfa220829037f9b5ccee9578b1fb5
Punit-Choudhary <itspunitchoudhary@gmail.com>
2021-03-11 00:59:37 +0530

    init

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,1 +1,27 @@
-# Code goes here!
+import sys
+import pygame
+
+def run_game():
+    # Initize game and creates a screen object
+    pygame.init()
+    screen = pygame.display.set_mode(1200, 800)
+    pygame.display.set_caption("Alien Invasion")
+
+    # Set the background color
+    bg_color = (230, 230, 230)
+
+    # Starts main loop for game
+    while True:
+
+        # Watch for keyboard and mouse events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        # Redraw the screen during each pass through the loop.
+        screen.fill(bg_color)
+
+        # Make screen visible
+        pygame.display.flip()
+
+run_game()
